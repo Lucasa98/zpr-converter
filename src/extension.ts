f77ed9aef92a60e96243bd3ef5681f27e89c6b7e
lucasa98 <lusaurin@gmail.com>
2025-08-10 16:07:40 -0300

    logica general implementada

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import { ZprConfig, parseZpr } from './parser';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,10 +16,124 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('zpr-converter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from zpr converter!');
+	const disposable = vscode.commands.registerCommand('zpr-converter.generate', async () => {
+		if (!vscode.workspace.workspaceFolders) {
+			vscode.window.showErrorMessage('Debe haber un workspace abierto.');
+			return;
+		}
+
+		// ======================= Conseguir ZPR =======================
+		// Buscar archivos zpr
+		const zprFiles = await vscode.workspace.findFiles('*.zpr');
+
+		if (zprFiles.length === 0) {
+			vscode.window.showErrorMessage('No se encontraron archivos ".zpr".');
+			return;
+		}
+
+		// Seleccionar un zpr
+		const pickedZpr = await vscode.window.showQuickPick(zprFiles.map(uri => uri.fsPath), {
+			placeHolder: 'project.zpr',
+			title: 'Elija el archivo zpr del proyecto de Zinjai',
+			canPickMany: false,
+			ignoreFocusOut: false
+		});
+		if (!pickedZpr) return;
+
+		// ======================= Conseguir CPP =======================
+		// Parsear zpr
+		const config: ZprConfig = parseZpr(pickedZpr);
+
+		// Seleccionar cpp
+		const cppFiles = await vscode.workspace.findFiles('*.cpp');
+		if (cppFiles.length === 0) {
+			vscode.window.showErrorMessage('No se encontraron archivos .cpp en la carpeta.');
+			return;
+		}
+		const pickedCppUri = cppFiles[0];
+
+		/* Lo ideal seria que se elija un entrypoint (main.cpp) para el proyecto. Por ahora lo dejo simple
+		if (config.source_paths.length > 0) {
+			const pickedCpp = config.source_paths[0];
+		}
+		const pickedCpp = await vscode.window.showQuickPick(config.source_paths, {
+			placeHolder: 'main.cpp',
+			title: 'Elija el archivo cpp para usar como entrypoint',
+			canPickMany: false,
+			ignoreFocusOut: false
+		});
+		if (!pickedCpp) return;
+
+		const pickedCppPath = vscode.Uri.joinPath(
+			vscode.Uri.file(path.dirname(pickedZpr)),
+			pickedCpp
+		);
+		*/
+
+		// ======================= Generar tasks.json =======================
+		// Llamar al comando de Cpp C_Cpp.AddDebugConfiguration para generar tasks.json
+		const doc = await vscode.workspace.openTextDocument(pickedCppUri);
+		const editor = await vscode.window.showTextDocument(doc, { preview: false });
+		await vscode.commands.executeCommand('C_Cpp.BuildAndDebugFile');
+
+		// ======================= Leer tasks.json =======================
+		const workspaceFolderFullPath = vscode.workspace.workspaceFolders[0].uri.fsPath;
+		const vscodeDir = path.join(workspaceFolderFullPath, '.vscode');
+		const tasksJsonPath = path.join(vscodeDir, 'tasks.json');
+		// Esperar hasta que aparezca el tasks.json (el comando es asincronico y con flujo controlado por el usuario)
+		await new Promise<void>((resolve, reject) => {
+			const folderWatcher = fs.watch(workspaceFolderFullPath, async (eventType, foldername) => {
+				if (foldername === '.vscode') {
+					await new Promise<void>((resolve, reject) => {
+						const fileWatcher = fs.watch(vscodeDir, (eventType, filename) => {
+							if (filename === 'tasks.json') {
+								fileWatcher.close();
+								resolve();
+							}
+						});
+					});
+					folderWatcher.close();
+					resolve();
+				}
+			});
+		});
+
+		// Leer tasks.json
+		const tasksJson = JSON.parse(fs.readFileSync(tasksJsonPath, 'utf8'));
+		const tasks = tasksJson.tasks;
+
+		if (!tasksJson) {
+			vscode.window.showErrorMessage('Ha ocurrido un error generando el "tasks.json".');
+			return;
+		}
+
+		if (!Array.isArray(tasks)) {
+			vscode.window.showErrorMessage('Ha ocurrido un error generando el "tasks.json".');
+			return;
+		}
+		const task = tasks.find(t => t.type == 'cppbuild');
+		if (!task) {
+			vscode.window.showErrorMessage('No se encontro task "cppbuild".');
+			return;
+		}
+
+		// ======================= Modificar tasks.json a partir de ZPR =======================
+		if (Array.isArray(task.args)) {
+			const insertIndex = task.args.indexOf('-g') + 1;
+			if (insertIndex > 0) {
+				task.args.splice(insertIndex, 0, `-std=${config.std_cpp || "c++11"}`);
+				task.args.splice(insertIndex+1, 0, "-I${fileDirname}");
+			}
+			// TODO: tener en cuenta mas parametros del .zpr
+		} else {
+			vscode.window.showErrorMessage('No se encontraron argumentos de ejecucion en "tasks.json".');
+			return;
+		}
+
+		// Guardar tasks.json
+		fs.writeFileSync(tasksJsonPath, JSON.stringify(tasksJson, null, 4));
+
+		vscode.window.showInformationMessage('tasks.json creado.');
 	});
 
 	context.subscriptions.push(disposable);
---
+++ src/parser.ts
@@ -0,0 +1,67 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export interface ZprConfig {
+    std_cpp?: string;
+    output_file?: string;
+    headers_dirs?: string[];
+    project_name?: string;
+    macros?: string[];
+    optimization_level?: string;
+    debug_level?: string;
+    compiling_extra?: string;
+    libraries_dirs?: string[];
+    libraries?: string[];
+    source_paths: string[];
+}
+
+function solveHeaderDirName(v: string): string {
+    if (!v) {
+        return ' ';
+    }
+
+    const [root, folder_name] = v.split(' ');
+
+    // No hace falta si esta en la carpeta raiz del proyecto
+    if (root == '.') {
+        return ' ';
+    }
+
+    return path.join(root,folder_name);
+}
+
+export function parseZpr(filePath: string): ZprConfig {
+    const config: ZprConfig = {source_paths: []};
+
+    // Leer .zpr
+    const content = fs.readFileSync(filePath, 'utf8');
+    for (const line of content.split(/\r?\n/)) {
+        // cada parametro y valor del zpr
+        const [key, value_comment] = line.split('=').map(elem => elem.trim());
+        // eliminar posible comentario inline
+        const value = value_comment?.split('#')[0];
+
+        if (!key || !value) continue;
+
+        // Leer parametros
+        switch (key.trim()) {
+            case 'std_cpp':
+                config.std_cpp = value.trim();
+                break;
+            case 'output_file':
+                config.output_file = value.trim();
+                break;
+            case 'headers_dirs':
+                config.headers_dirs = value.split(';').map(v => solveHeaderDirName(v)).filter(v => v != ' ');
+                break;
+            case 'project_name':
+                config.project_name = value.trim();
+                break;
+            case 'path':
+                config.source_paths.push(value.trim());
+            // agregar otros parametros
+        }
+    }
+
+    return config;
+}
